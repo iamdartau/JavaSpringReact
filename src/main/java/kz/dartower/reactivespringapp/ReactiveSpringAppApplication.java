8948a33fb227c0656e4c7c442271b9fb411995a6
Alex <iamdartaudeveloper@gmail.com>
2023-03-27 17:08:15 +0600

    init

---
+++ src/main/java/kz/dartower/reactivespringapp/ReactiveSpringAppApplication.java
@@ -0,0 +1,13 @@
+package kz.dartower.reactivespringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveSpringAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/dartower/reactivespringapp/ReactiveSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.dartower.reactivespringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSpringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
